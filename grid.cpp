da0c4c8e7d75616f83fec850dc3a9dcb244a686d
Ian Convy <ian_convy@berkeley.edu>
2019-05-28 22:22:18 -0700

    Pieces can now rotate through top of board

--- grid.cpp
+++ grid.cpp
@@ -13,18 +13,9 @@ bool Grid::collisionCheck(const std::vector<std::pair<int, int>> coords)
 {
     bool collision = false;
     for (auto& rowCol : coords) {
-        if (rowCol.first < 0 || rowCol.first >= height) {
+        bool outOfBounds = rowCol.first < 0 || rowCol.second < 0 || rowCol.second >= width;
+        if (outOfBounds || rowCol.first < height && grid[rowCol.first][rowCol.second]) {
             collision = true;
-        }
-        else {
-            if (rowCol.second < 0 || rowCol.second >= width) {
-                collision = true;
-            }
-            else {
-                if (grid[rowCol.first][rowCol.second]) {
-                    collision = true;
-                }
-            }
         }
     }
     return collision;
