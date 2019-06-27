4f779cec818772000435884671b4f1962c9c2e2e
Ian Convy <ian_convy@berkeley.edu>
2019-06-26 19:26:06 -0700

    Text characters now keep proper proportion

--- graphics/text.cpp
+++ graphics/text.cpp
@@ -49,24 +49,28 @@ charTexCoords{
     {'9', {3745, 3864}},
     {'-', {3865, 3960}},
     {' ', {3950, 3960}},
-    {'.', {3960, 3993}}}
+    {'.', {3960, 4066}}}
 {}
 
 std::vector<std::vector<float>> TextDrawer::getTextVertices(std::string text, float x0, float x1, float y0, float y1)
 {
-    float spacing = (x1 - x0) / text.length();
+    float horiz_spacing = (x1 - x0) / text.length();
+    float vert_spacing = horiz_spacing*(94.0/106.0);
+    float vert_offset = (y1 - y0 - vert_spacing);
+    float y0char = y0 + 0.5*vert_offset;
+    float y1char = y1 - 0.5*vert_offset;
     float charCount = 0;
     std::vector<std::vector<float>> textVertices;
     for (auto& c : text) {
-        float x0Char = x0 + spacing*charCount;
-        float x1Char = x0Char + spacing;
-        float x0Tex = charTexCoords[c][0] / 4000.0;
-        float x1Tex = charTexCoords[c][1] / 4000.0;
+        float x0Char = x0 + horiz_spacing*charCount;
+        float x1Char = x0Char + horiz_spacing;
+        float x0Tex = charTexCoords[c][0] / 4066.0;
+        float x1Tex = charTexCoords[c][1] / 4066.0;
         std::vector<float> charVertices{
-            x0Char, y1, 0,  x0Tex, 0,
-            x1Char, y1, 0,  x1Tex, 0,
-            x0Char, y0, 0,  x0Tex, 1,
-            x1Char, y0, 0,  x1Tex, 1};
+            x0Char, y1char, 0,  x0Tex, 0,
+            x1Char, y1char, 0,  x1Tex, 0,
+            x0Char, y0char, 0,  x0Tex, 1,
+            x1Char, y0char, 0,  x1Tex, 1};
         textVertices.push_back(charVertices);
         ++charCount;
     }
